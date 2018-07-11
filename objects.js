7e545e2f735c2103059252d772f8c877e174979e
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-11 14:02:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {'Aphex Twin': 'Cow Cud Is A Twin'}
 
 function updatePlaylist(obj, artist, song) {
-  return obj.[artist] = song
+  return obj[artist] = song
 }
 
 updatePlaylist(playlist, 'Bonobo', 'Eyesdown')
